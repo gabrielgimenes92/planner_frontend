d2dc038bd9837485d8e79080e08829abbed91d07
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-07-08 14:02:52 -0700

    Fix case where there are no habits to show

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -10,26 +10,41 @@ const Habits = ({
   setDayHabitsList,
   selectedDate,
 }) => {
+  // useEffect(() => {
+  //   for (let i = 0; i < monthHabitsList.length; i++) {
+  //     if (monthHabitsList[i].date.getDay() == selectedDate.getDay()) {
+  //       setDayHabitsList(monthHabitsList[i].habits);
+  //     }
+  //   }
+  // }, [selectedDate]);
+
   useEffect(() => {
-    for (let i = 0; i < monthHabitsList.length; i++) {
-      if (monthHabitsList[i].date.getDay() == selectedDate.getDay()) {
-        setDayHabitsList(monthHabitsList[i].habits);
-      }
+    let currentDaysHabits = monthHabitsList.find(
+      (o) => o.date.getTime() === selectedDate.getTime()
+    );
+    if (currentDaysHabits) {
+      setDayHabitsList(currentDaysHabits.habits);
+    } else {
+      setDayHabitsList('');
     }
-  }, []);
+  }, [selectedDate]);
 
   return (
     <div>
-      <ul className={styles.habits}>
-        {dayHabitsList.map((habit) => (
-          <Habit
-            id={habit.id}
-            description={habit.description}
-            color={habit.color}
-            checked={habit.checked}
-          />
-        ))}
-      </ul>
+      {dayHabitsList != '' ? (
+        <ul className={styles.habits}>
+          {dayHabitsList.map((habit) => (
+            <Habit
+              id={habit.id}
+              description={habit.description}
+              color={habit.color}
+              checked={habit.checked}
+            />
+          ))}
+        </ul>
+      ) : (
+        <h1>Nothing here</h1>
+      )}
     </div>
   );
 };
